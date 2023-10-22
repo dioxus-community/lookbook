5d696f3cce6c76bbb883123f3060cb489732127a
Matt Hunzinger <matthunz2@gmail.com>
2023-10-22 02:31:04 -0400

    Add controls gap

--- src/ui/look.rs
+++ src/ui/look.rs
@@ -8,19 +8,35 @@ pub fn Look<'a>(
     controls: Element<'a>,
     children: Element<'a>,
 ) -> Element<'a> {
-    render!(
-        div { flex: 1, display: "flex", flex_direction: "column",
-            VerticalPane {
-                top: render!(
-                    div { padding : "20px", h2 { "{name}" } } div { flex : 1, display : "flex",
-                    flex_direction : "column", padding : "20px", display : "flex", justify_content :
-                    "center", align_items : "center", div { flex : 1, children } }
-                ),
-                bottom: render!(
-                    div { flex : 1, overflow_y : "auto", padding : "20px", background : "#f9f9f9", h4 {
-                    "Controls" } controls }
-                )
-            }
+    let top = render!(
+        div { padding: "20px", h2 { "{name}" } }
+        div {
+            flex: 1,
+            display: "flex",
+            flex_direction: "column",
+            padding: "20px",
+            display: "flex",
+            justify_content: "center",
+            align_items: "center",
+            div { flex: 1, children }
         }
+    );
+
+    let bottom = render!(
+        div {
+            flex: 1,
+            display: "flex",
+            flex_direction: "column",
+            overflow_y: "auto",
+            padding: "20px",
+            gap: "20px",
+            background: "#f9f9f9",
+            h4 { "Controls" }
+            controls
+        }
+    );
+
+    render!(
+        div { flex: 1, display: "flex", flex_direction: "column", VerticalPane { top: top, bottom: bottom } }
     )
 }
